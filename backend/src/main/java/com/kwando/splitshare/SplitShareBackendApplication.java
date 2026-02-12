ab58a29f30bb58130260d61151870ce92a89de76
Kwandokuhle Sibiya <kwandoh.sibiya11@gmail.com>
2026-02-12 14:07:20 +0200

    Add initial Spring Boot backend skeleton with security

---
+++ backend/src/main/java/com/kwando/splitshare/SplitShareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kwando.splitshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitShareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitShareBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kwando/splitshare/SplitShareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwando.splitshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitShareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
